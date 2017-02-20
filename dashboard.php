4bd032c422733c1ab1e8d24055c9b4ebf22b3986
Justin Paulin <hello@justinpaulin.com>
2017-02-20 18:16:21 +0800

    Added lots of skillz

--- dashboard.php
+++ dashboard.php
@@ -87,6 +87,8 @@ $description = $profile['description'];
 $social = json_decode($profile['social'], true);
 $skills = json_decode($profile['skills']);
 $links = json_decode($profile['links'], true);
+
+$possibleSkills = getPossibleSkills($connection);
 ?>
 <!DOCTYPE html>
 <html>
@@ -124,7 +126,8 @@ $links = json_decode($profile['links'], true);
 
                     <div class="field">
                         <label>Subheading: </label>
-                        <input name="subtitle" type="text" placeholder="Your Subheading (required)" value="<?php echo $subtitle; ?>">
+                        <input name="subtitle" type="text" placeholder="Your Subheading (required)"
+                               value="<?php echo $subtitle; ?>">
                     </div>
 
                     <div class="field">
@@ -134,205 +137,27 @@ $links = json_decode($profile['links'], true);
                         <div class="ui label">
                             http://
                         </div>
-                        <input name="image" type="text" placeholder="imgur.com/your_picture.jpeg" value="<?php if (substr($image, 0, 1) != 'i') { echo ltrim($image, 'http://'); } ?>">
+                        <input name="image" type="text" placeholder="imgur.com/your_picture.jpeg"
+                               value="<?php if (substr($image, 0, 1) != 'i') {
+                                   echo ltrim($image, 'http://');
+                               } ?>">
                     </div>
 
                     <div class="field">
                         <label>Summary</label>
-                        <textarea name="summary" rows="2" placeholder="Your Summary (required)"><?php echo $description; ?></textarea>
+                        <textarea name="summary" rows="2"
+                                  placeholder="Your Summary (required)"><?php echo $description; ?></textarea>
                     </div>
 
                     <div class="field">
                         <label>Skills</label>
                         <select name="skills[]" class="ui fluid search dropdown" id="skill-select" multiple="">
                             <option value="">Your Skills</option>
-                            <option value="Iphone Sdk">Iphone Sdk</option>
-                            <option value="Jade">Jade</option>
-                            <option value="Jasmine">Jasmine</option>
-                            <option value="Java">Java</option>
-                            <option value="JavaScript">JavaScript</option>
-                            <option value="Jira">Jira</option>
-                            <option value="jQuery">jQuery</option>
-                            <option value="JSON">JSON</option>
-                            <option value="jsoup">jsoup</option>
-                            <option value="Jsp">Jsp</option>
-                            <option value="Kinect">Kinect</option>
-                            <option value="Laravel">Laravel</option>
-                            <option value="Latex">Latex</option>
-                            <option value="Less">Less</option>
-                            <option value="Libgdx">Libgdx</option>
-                            <option value="LinkedIn">LinkedIn</option>
-                            <option value="Linux">Linux</option>
-                            <option value="Lua">Lua</option>
-                            <option value="Machine Learning">Machine Learning</option>
-                            <option value="Magento">Magento</option>
-                            <option value="MapKit">MapKit</option>
-                            <option value="Markdown">Markdown</option>
-                            <option value="Matlab">Matlab</option>
-                            <option value="Maven">Maven</option>
-                            <option value="Memcached">Memcached</option>
-                            <option value="MongoDB">MongoDB</option>
-                            <option value="Mongoose">Mongoose</option>
-                            <option value="Monodevelop">Monodevelop</option>
-                            <option value="MySQL">MySQL</option>
-                            <option value="Neo4j">Neo4j</option>
-                            <option value="Netbeans">Netbeans</option>
-                            <option value="NFC">NFC</option>
-                            <option value="Nginx">Nginx</option>
-                            <option value="Node.js">Node.js</option>
-                            <option value="Notepad++">Notepad++</option>
-                            <option value="Npm">Npm</option>
-                            <option value="NumPy">NumPy</option>
-                            <option value="Oauth">Oauth</option>
-                            <option value="Objective C">Objective C</option>
-                            <option value="OpenCV">OpenCV</option>
-                            <option value="OpenGL">OpenGL</option>
-                            <option value="OpenLayers">OpenLayers</option>
-                            <option value="Pandas">Pandas</option>
-                            <option value="PayPal">PayPal</option>
-                            <option value="Perl">Perl</option>
-                            <option value="PhantomJS">PhantomJS</option>
-                            <option value="Phonegap">Phonegap</option>
-                            <option value="Photoshop">Photoshop</option>
-                            <option value="PHP">PHP</option>
-                            <option value="Phpmyadmin">Phpmyadmin</option>
-                            <option value="Postgresql">Postgresql</option>
-                            <option value="Powerpoint">Powerpoint</option>
-                            <option value="Processing">Processing</option>
-                            <option value="Pygame">Pygame</option>
-                            <option value="Python">Python</option>
-                            <option value="Qt">Qt</option>
-                            <option value="R">R</option>
-                            <option value="Raspberry Pi">Raspberry Pi</option>
-                            <option value="Redis">Redis</option>
-                            <option value="Rspec">Rspec</option>
-                            <option value="Ruby">Ruby</option>
-                            <option value="Ruby on Rails">Ruby on Rails</option>
-                            <option value="RubyGems">RubyGems</option>
-                            <option value="Scala">Scala</option>
-                            <option value="SciPy">SciPy</option>
-                            <option value="Sdl">Sdl</option>
-                            <option value="Selenium">Selenium</option>
-                            <option value="Sencha">Sencha</option>
-                            <option value="Symfony">Symfony</option>
-                            <option value="Telerik">Telerik</option>
-                            <option value="three.js">three.js</option>
-                            <option value="Titanium">Titanium</option>
-                            <option value="Tomcat">Tomcat</option>
-                            <option value="Tornado">Tornado</option>
-                            <option value="UIKit">UIKit</option>
-                            <option value="UnderscoreJS">UnderscoreJS</option>
-                            <option value="Vim">Vim</option>
-                            <option value="Visual Studio">Visual Studio</option>
-                            <option value="Webgl">Webgl</option>
-                            <option value="Windows Phone">Windows Phone</option>
-                            <option value="WiX">WiX</option>
-                            <option value="Word">Word</option>
-                            <option value="WordPress">WordPress</option>
-                            <option value="Xamarin">Xamarin</option>
-                            <option value="XAML">XAML</option>
-                            <option value="Xcode">Xcode</option>
-                            <option value="Xna">Xna</option>
-                            <option value="Yii">Yii</option>
-                            <option value="YouTube">YouTube</option>
-                            <option value="Zurb Foundation">Zurb Foundation</option>
-                            <option value="All4webs">All4webs</option>
-                            <option value="Twitter">Twitter</option>
-                            <option value="OpenXC">OpenXC</option>
-                            <option value="Twilio">Twilio</option>
-                            <option value="gamesalad">gamesalad</option>
-                            <option value="HTML5">HTML5</option>
-                            <option value="bootstrap">bootstrap</option>
-                            <option value="bit.ly">bit.ly</option>
-                            <option value="domainr">domainr</option>
-                            <option value="OpenShift">OpenShift</option>
-                            <option value="Moment.Js">Moment.Js</option>
-                            <option value=".NET">.NET</option>
-                            <option value="Crashlytics">Crashlytics</option>
-                            <option value="Bloomberg">Bloomberg</option>
-                            <option value="League of Legends">League of Legends</option>
-                            <option value="Mac">Mac</option>
-                            <option value="Lob">Lob</option>
-                            <option value="Core Foundation">Core Foundation</option>
-                            <option value="Firebase">Firebase</option>
-                            <option value="Flickr">Flickr</option>
-                            <option value="imgur">imgur</option>
-                            <option value="Impress.Js">Impress.Js</option>
-                            <option value="dropzone.js">dropzone.js</option>
-                            <option value="Lamp">Lamp</option>
-                            <option value="namecheap">namecheap</option>
-                            <option value="Sendgrid">Sendgrid</option>
-                            <option value="Nest">Nest</option>
-                            <option value="Leap Motion">Leap Motion</option>
-                            <option value="Overtone">Overtone</option>
-                            <option value="Google Cloud Messaging">Google Cloud Messaging</option>
-                            <option value="Google Compute Engine">Google Compute Engine</option>
-                            <option value="Nltk">Nltk</option>
-                            <option value="wikipedia">wikipedia</option>
-                            <option value="moodstocks">moodstocks</option>
-                            <option value="Css3">Css3</option>
-                            <option value="Dreamweaver">Dreamweaver</option>
-                            <option value="tastypie">tastypie</option>
-                            <option value="evernote">evernote</option>
-                            <option value="Snapchat">Snapchat</option>
-                            <option value="mashape">mashape</option>
-                            <option value="Instagram">Instagram</option>
-                            <option value="CodeMirror">CodeMirror</option>
-                            <option value="Gracenote">Gracenote</option>
-                            <option value="WhoSampled">WhoSampled</option>
-                            <option value="Linode">Linode</option>
-                            <option value="Creative Commons">Creative Commons</option>
-                            <option value="Google+">Google+</option>
-                            <option value="Rdio">Rdio</option>
-                            <option value="mashery">mashery</option>
-                            <option value="zxing">zxing</option>
-                            <option value="Dwolla">Dwolla</option>
-                            <option value="Google Directions">Google Directions</option>
-                            <option value="Weather Underground">Weather Underground</option>
-                            <option value="Stripe">Stripe</option>
-                            <option value="Yelp">Yelp</option>
-                            <option value="23andme">23andme</option>
-                            <option value="Filepicker.io">Filepicker.io</option>
-                            <option value="CrunchBase">CrunchBase</option>
-                            <option value="Chrome">Chrome</option>
-                            <option value="Android Studio">Android Studio</option>
-                            <option value="TypeScript">TypeScript</option>
-                            <option value="soundcloud">soundcloud</option>
-                            <option value="Xml">Xml</option>
-                            <option value="OpenTok">OpenTok</option>
-                            <option value="iBeacon">iBeacon</option>
-                            <option value="Square">Square</option>
-                            <option value="Nvidia">Nvidia</option>
-                            <option value="google books">google books</option>
-                            <option value="jinja">jinja</option>
-                            <option value="Express.Js">Express.Js</option>
-                            <option value="Berkeley Nlp Tools">Berkeley Nlp Tools</option>
-                            <option value="DigitalOcean">DigitalOcean</option>
-                            <option value="venmo">venmo</option>
-                            <option value="Google Glass">Google Glass</option>
-                            <option value="Appinions">Appinions</option>
-                            <option value="Rotten Tomatoes">Rotten Tomatoes</option>
-                            <option value="Google Web Speech API">Google Web Speech API</option>
-                            <option value="MEAN">MEAN</option>
-                            <option value="ESPN">ESPN</option>
-                            <option value="USA Today">USA Today</option>
-                            <option value="last.fm">last.fm</option>
-                            <option value="spotify">spotify</option>
-                            <option value="Love">Love</option>
-                            <option value="Clickslide">Clickslide</option>
-                            <option value="zillow">zillow</option>
-                            <option value="Google chart">Google chart</option>
-                            <option value="Ushahidi">Ushahidi</option>
-                            <option value="balsamiq">balsamiq</option>
-                            <option value="USPS Web Tools API">USPS Web Tools API</option>
-                            <option value="UPS Tracking API">UPS Tracking API</option>
-                            <option value="Scss">Scss</option>
-                            <option value="Resque">Resque</option>
-                            <option value="PhysicsJS">PhysicsJS</option>
-                            <option value="Project Anarchy">Project Anarchy</option>
-                            <option value="Blender">Blender</option>
-                            <option value="Tesseract">Tesseract</option>
-                            <option value="Bing Translator">Bing Translator</option>
+                            <?php
+                            foreach ($possibleSkills as $possibleSkill) {
+                                echo '<option value="' . $possibleSkill . '">' . $possibleSkill . '</option>';
+                            }
+                            ?>
                         </select>
                     </div>
 
--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 13, 2017 at 06:38 PM
+-- Generation Time: Feb 20, 2017 at 06:15 PM
 -- Server version: 5.5.54-0+deb8u1
--- PHP Version: 5.6.29-0+deb8u1
+-- PHP Version: 5.6.30-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,19 @@ CREATE TABLE IF NOT EXISTS `pw_reset` (
   `email` varchar(255) NOT NULL,
   `hash` varchar(40) NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+CREATE TABLE IF NOT EXISTS `skills` (
+`id` int(11) NOT NULL,
+  `skill` varchar(250) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11205 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -50,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `account_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_login` timestamp NULL DEFAULT NULL,
   `notes` text
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -61,6 +73,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 ALTER TABLE `pw_reset`
  ADD PRIMARY KEY (`id`), ADD KEY `email` (`email`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -76,9 +94,14 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `pw_reset`
 --
 ALTER TABLE `pw_reset`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11205;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
